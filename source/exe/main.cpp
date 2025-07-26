065aa0b2287728d140b37267f6e403c09866efac
Jumpei Coder <shirono.linux@gmail.com>
2025-07-27 04:44:25 +0900

    Simplified CMakeLists.txt

--- source/exe/main.cpp
+++ source/exe/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+//#include "Mylib.hpp"
+#include <Eigen/Dense>
 using namespace std;
+using namespace Eigen;
 
 
 int main() {
-    cout << "Hello world" << endl;
-    return 1;
+//    cout << "Hello world" << endl;
+//    Mylib mylib;
+//    cout << mylib.func() << endl;
+//    return 1;
+
+    Vector3d v1(1.0, 2.0, 3.0);
+    Vector3d v2(1.0, 2.0, 3.0);
+    cout << v1 + v2 << ", " << v1 - v2 << ", " << endl;
+
+    
 }
