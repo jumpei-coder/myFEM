5cadd97b1def1d49829729b9791a4775c6b9deec
Jumpei Coder <shirono.linux@gmail.com>
2025-07-27 04:50:45 +0900

    Check vector operator in Eigen

--- source/exe/main.cpp
+++ source/exe/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-//#include "Mylib.hpp"
+#include "Mylib.hpp"
 #include <Eigen/Dense>
 using namespace std;
 using namespace Eigen;
 
 
 int main() {
-//    cout << "Hello world" << endl;
-//    Mylib mylib;
-//    cout << mylib.func() << endl;
-//    return 1;
+    cout << "Hello world" << endl;
+    Mylib mylib;
+    cout << mylib.func() << endl;
 
     Vector3d v1(1.0, 2.0, 3.0);
     Vector3d v2(1.0, 2.0, 3.0);
     cout << v1 + v2 << ", " << v1 - v2 << ", " << endl;
+
+    cout << v1.norm() << endl;
+    cout << v1.dot(v2) << endl;
+    cout << v1.cross(v2) << endl;
+    cout << v1.array() * v2.array() << endl;
 
     
 }
